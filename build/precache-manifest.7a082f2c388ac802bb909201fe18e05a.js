d559131f71b825d2766b8de85ba534bf279fe2fa
nawelkh <khelilnawel11@gmail.com>
2021-03-26 21:54:34 +0100

    f

---
+++ build/precache-manifest.7a082f2c388ac802bb909201fe18e05a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6dd2e2801aac43e248b",
+    "url": "/static/css/main.841a5c4b.chunk.css"
+  },
+  {
+    "revision": "b6dd2e2801aac43e248b",
+    "url": "/static/js/main.16ca9d81.chunk.js"
+  },
+  {
+    "revision": "5954c9e456a98eb52933",
+    "url": "/static/js/runtime~main.0d64a074.js"
+  },
+  {
+    "revision": "c027221007a99ed2982b",
+    "url": "/static/js/2.d93238a9.chunk.js"
+  },
+  {
+    "revision": "35ee8791a13dfc780d9a",
+    "url": "/static/js/3.4c1ad2bd.chunk.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "f02898ea1c6d97998f97dd3f029902fd",
+    "url": "/index.html"
+  }
+];
